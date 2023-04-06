ee7605949f5a449beb24c7b1ead3dd4230212ad9
mathferreira01 <ferreiramatheus124@gmail.com>
2023-04-06 00:13:40 -0300

    feat: Structuring the project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express';
+import { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.route("/").get((Request, res: Response) => {
-  res.status(200).send({ title: "API Menu" });
+app.route('/').get((Request, res: Response) => {
+   res.status(200).send({ title: 'API Menu' });
 });
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log("Server is running on PORT 3000"));
+app.listen(PORT, () => console.log('Server is running on PORT 3000'));
