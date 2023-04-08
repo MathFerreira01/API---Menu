10107d114deb0550b1ba918338a35d5e33ec1f08
mathferreira01 <ferreiramatheus124@gmail.com>
2023-04-08 11:08:02 -0300

    refactor: correcting error when searching get product by id

--- src/repositories/product/findById-product-repository.ts
+++ src/repositories/product/findById-product-repository.ts
@@ -4,7 +4,12 @@ import products from '../../models/product-model';
 const FindByIdProductRepository = async (req: Request) => {
    const id = req.params.id;
 
-   products.findById(id).populate('categories', 'name').exec();
+   const dataProductById = products
+      .findById(id)
+      .populate('categories')
+      .exec();
+
+   return dataProductById;
 };
 
 export default FindByIdProductRepository;
