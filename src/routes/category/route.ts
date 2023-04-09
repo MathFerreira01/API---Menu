f9257bf98431595c30937d96ff9ea254add2c76e
mathferreira01 <ferreiramatheus124@gmail.com>
2023-04-09 12:00:58 -0300

    Chore: delete files to create a new category

--- src/routes/category/route.ts
+++ src/routes/category/route.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import createCategoryController from '../../controllers/category/create-category-controller';
 import allCategoriesController from '../../controllers/category/all-category-controller';
 
 const routeCategory = Router();
 
-routeCategory
-   .post('/category', createCategoryController)
-   .get('/category', allCategoriesController);
+routeCategory.get('/category', allCategoriesController);
 
 export { routeCategory };
