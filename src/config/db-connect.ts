dc9d898a75be5e46ebde1e2a37a75f575fceeada
mathferreira01 <ferreiramatheus124@gmail.com>
2023-04-09 11:51:16 -0300

    refactor: changing dotenv library configuration

--- src/config/db-connect.ts
+++ src/config/db-connect.ts
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const DATABASE_URL = 'mongodb+srv://matheus:matheus12345@cluster0.0ujlzjd.mongodb.net/api-menu'
+dotenv.config();
+
+const DATABASE_URL =
+   'mongodb+srv://matheus:matheus12345@cluster0.0ujlzjd.mongodb.net/api-menu';
 
 const connectDB = async () => {
    try {
-      await mongoose.connect(DATABASE_URL);
+      await mongoose.connect(process.env.DATABASE_URL as string);
       console.log('Database connection successful');
    } catch (error) {
       console.log('Database connection error');
